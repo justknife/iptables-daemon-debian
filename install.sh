06642371eac5d21fcfa59b1ca96e7cc8b00d40dc
justknife <hunter.ixm@gmail.com>
2021-06-11 14:24:18 +0300

    fix execute

--- install.sh
+++ install.sh
@@ -7,6 +7,5 @@ mv ./functions /etc/init.d/
 iptables-save > /etc/iptables
 mkdir /usr/lib/iptables
 mv ./iptables.init /usr/lib/iptables
-chmod +x iptables.init
 systemctl daemon-reload
 systemctl start iptables
