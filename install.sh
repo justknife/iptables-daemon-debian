75dbcfd523ece2b7629d382ab0d93faf000ba0f4
justknife <hunter.ixm@gmail.com>
2021-06-11 14:02:59 +0300

    add install.sh edit path

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+# Only debian supported
 
+mv ./iptables.service /etc/systemd/system/multi-user.target.wants/
+mv ./functions /etc/init.d/
+iptables-save > /etc/iptables
+mkdir /usr/lib/iptables
+mv ./iptables.init /usr/lib/iptables
+systemctl daemon-reload
+systemctl start iptables
